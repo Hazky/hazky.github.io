7bbe0e15d8cb259d2181155053609ceeb9cc43f2
Przemysław Żydek <przemek543@gmail.com>
2016-07-08 10:58:14 +0200

    Dodanie odnośników, oddzielny folder na skrypty

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+  /*Mapa*/
+  function init() {
+    var mapProperties = {
+      center:new google.maps.LatLng(51.508742, -0.120850),
+      zoom: 16, 
+      mapTypeId:google.maps.MapTypeId.ROADMAP,
+     };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProperties);
+
+    map.addListener('click', function(){
+      if (map) map.setOptions({scrollwheel: true});
+    });
+
+    map.addListener('mouseout', function(){
+      if (map) map.setOptions({scrollwheel: false})
+    })
+  }
+
+  google.maps.event.addDomListener(window, 'load', init);
+
+/*Smooth scroll po kliknięciu na odnośnik w navbarze*/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/*Podświetlenie aktywnego elementu w navbarze*/
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
