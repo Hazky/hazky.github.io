b4464b111217ab45fa0e9f5db91ffaf58445fa79
Hazky <astrav300@gmail.com>
2016-10-12 16:43:09 +0200

    Naprawienie skryptu na smooth scrolla, pare estetycznych poprawek

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,34 @@
   //Mapa
   function init() {
     var mapProperties = {
-      center:new google.maps.LatLng(50.23109, 18.98474),
-      zoom: 18, 
-      mapTypeId:google.maps.MapTypeId.ROADMAP,
+      center: new google.maps.LatLng(50.23109, 18.98474),
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
      };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProperties);
 
-    map.addListener('click', function(){
+    map.addListener('click', function () {
       if (map) map.setOptions({scrollwheel: true});
     });
 
     map.addListener('mouseover', function(){
       if (map) map.setOptions({scrollwheel: false})
-    })
+    });
   }
 
   google.maps.event.addDomListener(window, 'load', init);
 
 //Smooth scroll po kliknięciu na odnośnik w navbarze
-$(function() {
-  $('.navlink:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
 });
 
 //Podświetlenie aktywnego elementu w navbarze
-$(".nav a").on("click", function(){
+$(".nav a").on("click", function () {
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
 });
